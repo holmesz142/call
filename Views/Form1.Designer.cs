fdf02fea1ccaf7ebcedead94b0a16a6364a812ff
holmesz <67871687+holmesz142@users.noreply.github.com>
2020-10-29 00:18:11 +0700

    Initial commit

---
+++ Views/Form1.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace call
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.searchUser = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 107);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(776, 331);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // searchUser
+            // 
+            this.searchUser.Location = new System.Drawing.Point(12, 39);
+            this.searchUser.Name = "searchUser";
+            this.searchUser.Size = new System.Drawing.Size(776, 20);
+            this.searchUser.TabIndex = 1;
+            this.searchUser.TextChanged += new System.EventHandler(this.searchUser_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.searchUser);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.TextBox searchUser;
+    }
+}
+
---
+++ Views/Form1.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace call
+{
+    public partial class Form1 : Form
+    {
+        List<User> listUser = new List<User>();
+        public Form1()
+        {
+            InitializeComponent();
+            listUser.Add(new User() { ID = 1, userName = "pham tan dat", dept = "cntt", numberPhone = "12315645" });
+            listUser.Add(new User() { ID = 2, userName = "tran quoc dai", dept = "cntt", numberPhone = "12315645" });
+            listUser.Add(new User() { ID = 3, userName = "duong quoc huy", dept = "cntt", numberPhone = "12315645" });
+            listUser.Add(new User() { ID = 4, userName = "dao hong vinh", dept = "ktmt", numberPhone = "12315645" });
+            listUser.Add(new User() { ID = 5, userName = "phung vinh duc", dept = "ktdl", numberPhone = "12315645" });
+            listUser.Add(new User() { ID = 6, userName = "huynh thanh tam", dept = "abcd", numberPhone = "12315645" });
+            listUser.Add(new User() { ID = 10, userName = "phung cong chien", dept = "edgh", numberPhone = "12315645" });
+            
+            
+        }
+
+
+        private void searchUser_TextChanged(object sender, EventArgs e)
+        {
+            List<User> listUser2 = new List<User>();
+            if (string.IsNullOrEmpty(searchUser.Text) == false)
+            {
+                foreach(User user in listUser)
+                {
+                    if (user.dept.StartsWith(searchUser.Text))
+                    {
+                        listUser2.Add(user);
+                    }
+                }
+                dataGridView1.DataSource = listUser2;
+            }
+            else if (searchUser.Text == "")
+            {
+                foreach(User user in listUser)
+                {
+                    listUser2.Add(user);
+                }
+                dataGridView1.DataSource = listUser2;
+            }
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = listUser;
+
+        }
+    }
+    class User
+    {
+        public int ID { get; set; }
+        public string userName { get; set; }
+        public string dept { get; set; }
+        public string numberPhone { get; set; }
+    }
+}
